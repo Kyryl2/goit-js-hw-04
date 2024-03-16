81e777aef2e7fdd89403c5936d1daf5b979a264d
Kyryl2 <krasiuk.4884@gmail.com>
2024-03-16 20:46:01 +0100

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,32 +1,30 @@
-// Задача 1. Генератор slug
+// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
 
-// Виконуй це завдання у файлі task-1.js
+// Функція оголошує два параметри:
 
-// Перш, ніж розв’язувати задачу, давай визначимося із новим терміном!
+// products — об’єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів. Наприклад, { apples: 2, grapes: 4 }.
+// containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
+// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
 
-// Термін slug — це зрозумілий людині унікальний ідентифікатор, який використовується у веб розробці для створення читабельних URL-адрес.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
 
-// Наприклад, замість того, щоб користувач побачив в адресному рядку mysite.com/posts/1q8fh74tx, можна зробити slug із назви статті. У результаті адреса буде приємнішою для сприйняття: mysite.com/posts/arrays-for-begginers.
+function isEnoughCapacity(products, containerSize) {
+  const productsArray = Object.values(products);
+  let totalProducts = 0;
+  for (const value of productsArray) {
+    totalProducts += value;
+  }
+  if (totalProducts <= containerSize) {
+    return true;
+  }
 
-// Slug — це завжди рядок у нижньому регістрі, слова якого розділені тире.
-
-// З цим розібралися? А тепер давай нарешті виконувати задачу!
-
-// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
-
-// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
-// Усі символи slug повинні бути в нижньому регістрі.
-// Усі слова slug повинні бути розділені тире.
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-function slugify(title) {
-  const slug = title.toLowerCase();
-  const words = slug.split(' ');
-  const newString = words.join('-');
-  return newString;
+  return false;
 }
 
-console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
